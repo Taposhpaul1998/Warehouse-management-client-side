38cd3cb240596596877d63cf342a7b590bbad8bd
Taposh paul <taposhp05@gamil.com>
2022-05-07 15:37:16 +0600

    added footer section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './pages/Footer/Footer';
 import Header from './pages/Header/Header';
 import Home from './pages/Home/Home/Home';
 import Login from './pages/Login/Login/Login';
 
 function App() {
   return (
- <>
- <Header></Header>
- <Routes>
-   <Route path="/" element={<Home></Home>}></Route>
-   <Route path="/signin" element={<Login></Login>}></Route>
- </Routes>
- </>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/signin" element={<Login></Login>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
   );
 }
 
--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import {  Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './Header.css'
+import './Header.css';
 
 const Header = () => {
     return (
-        <Navbar style={{background:" #08093ed9"}}>
-          <div className="conteiner">
-              <div className="navIcon">
-                  <h4>Cycle House</h4>
-              </div>
-              <div className="navMenu">
-                  <Link to="/">HOME</Link>
-                  <Link to="/services">SERVICES</Link>
-                  <Link to="/blogs">BLOGS</Link>
-                  <Link to="/invertory">INVERTORY</Link>
-                  <Link to="/signup">SIGNUP</Link>
-                  <Link to="/signin">SIGNIN</Link>
-              </div>
-          </div>
+        <Navbar style={{ background: " #08093ed9" }}>
+            <div className="conteiner">
+                <div className="navIcon">
+                    <h4>Cycle House</h4>
+                </div>
+                <div className="navMenu">
+                    <Link to="/">HOME</Link>
+                    <Link to="/services">SERVICES</Link>
+                    <Link to="/blogs">BLOGS</Link>
+                    <Link to="/invertory">INVERTORY</Link>
+                    <Link to="/signup">SIGNUP</Link>
+                    <Link to="/signin">SIGNIN</Link>
+                </div>
+            </div>
         </Navbar>
     );
 };
